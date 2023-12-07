c45f53f59f52d0ab2eb61ad7dc657981f9c2adbd
windy-worm <31342528+windy-worm@users.noreply.github.com>
2023-12-07 14:46:29 +0000

    Initial commit

---
+++ EngineBay.ModuleTemplate.Tests/ModuleTemplateTests.cs
@@ -0,0 +1,13 @@
+namespace EngineBay.ModuleTemplate.Tests
+{
+    using Xunit;
+
+    public class ModuleTemplateTests
+    {
+        [Fact]
+        public void ControlTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
